798ea4514e75d87d328a6497bbfe8530b4d30acf
Dr. Sunetra Sarkar <Dr. Sunetra Sarkar>
2016-06-13 20:41:00 +0100

    Added google analytics snippet to wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -160,6 +160,7 @@ def application(env, start_response):
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+
 <script type="text/javascript">
 $(document).ready(function() {
     $('#xwd-table').DataTable( {
@@ -172,6 +173,19 @@ $(document).ready(function() {
     } );
 } );
 </script>
+
+<!-- Google Analytics snippet -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-79272811-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>THCParse</title>
 </head>
